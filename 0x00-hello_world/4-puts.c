53ce84e4bf3ac845a1ccda61d1d578848a24774d
TosinOgunro <ogunro.tosin97@gmail.com>
2022-12-08 12:30:05 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -11,5 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
-
-}	
+}
