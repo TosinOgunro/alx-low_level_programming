aebdfb203816d6a763778fc358db8db46c7b44b5
TosinOgunro <ogunro.tosin97@gmail.com>
2022-12-09 17:40:29 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  prints all possible different combinations of three digits
+ * main -  prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
  *
@@ -11,30 +11,25 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	int hundreds ='0';
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (tens = '0'; tens <= '9'; tens++) /*print tens digit*/
 	{
-		for (tens = '0'; tens <= '9'; tens++)
+		for (ones = '0'; ones <= '9'; ones++) /*print ones digit*/
 		{
-			for (ones = '0'; ones <= '9'; ones++)
+			if (!((ones == tens) || (tens > ones))) /*eliminate repitition*/
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens >ones) || (hundreds > tens))) /*eliminate repitition*/
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds commas and spaces*/
 				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' &&
-								tens == '8'))/*adds commas and spaces*/
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
+
 	putchar('\n');
+	
 	return (0);
 }
